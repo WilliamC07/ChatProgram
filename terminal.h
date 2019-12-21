a990ec86a8599d6a38465d82fd426fd211141c9e
William Cao <wcao00@stuy.edu>
2019-12-21 16:53:08 -0500

    created enum to divide up screen

--- terminal.h
+++ terminal.h
@@ -1,3 +1,25 @@
+/**
+ * Divides the terminal into 4 sections
+ *
+ * TOP:
+ * - 1 line
+ * - Right Aligned: <IP ADDRESS> | <PORT>
+ * - Left Aligned: <error message if any>
+ *
+ * MIDDLE:
+ * - Rest of lines (after compensating for TOP, MESSAGE, and COMMAND section
+ *
+ * BOTTOM:
+ * - 2 lines
+ *
+ */
+enum Section {TOP, MIDDLE, BOTTOM};
+
 void enter_raw_mode();
 
 void disable_raw_mode();
+
+/**
+ * Clears what is currently displayed on the terminal
+ */
+void clear_terminal();
