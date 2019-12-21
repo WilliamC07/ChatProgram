0547a299b0df8fdd3db09d328857208d02ca5870
William Cao <wcao00@stuy.edu>
2019-12-21 15:50:42 -0500

    refractor key press processing

--- handle_input.c
+++ handle_input.c
@@ -11,75 +11,78 @@ size_t length_message = -1;
 // Command mode is when the user clicks ESCAPE
 bool on_command_mode = false;
 
-void handle_input(char input){
-    if(input == '\0') {
-        // No input given
-        return;
-    }else if(input == 27){
-        // Clicked ESCAPE button or arrow keys
-        char escape_sequence[2];
-        if(read(STDIN_FILENO, escape_sequence, sizeof(char)) == -1) escape_sequence[0] = 0;
-        if(read(STDIN_FILENO, escape_sequence + 1, sizeof(char)) == -1) escape_sequence[1] = 0;
+void handle_escape(){
+    char escape_sequence[2];
+    if(read(STDIN_FILENO, escape_sequence, sizeof(char)) == -1) escape_sequence[0] = 0;
+    if(read(STDIN_FILENO, escape_sequence + 1, sizeof(char)) == -1) escape_sequence[1] = 0;
 
-        if(escape_sequence[0] == 0){
-            on_command_mode = true;
-            printf("Switched to command move\n");
-        }else if(escape_sequence[0] == '['){
-            switch(escape_sequence[1]){
-                case 'A':
-                    // up arrow
-                    printf("up arrow\r\n");
-                    break;
-                case 'C':
-                    // right arrow
-                    printf("Right arrow\r\n");
-                    break;
-                case 'B':
-                    // down arrow
-                    printf("down arrow\r\n");
-                    break;
-                case 'D':
-                    // left arrow
-                    printf("left arrow\r\n");
-                    break;
-                default:
-                    printf("%d - %d - %d\r\n", input, escape_sequence[0], escape_sequence[1]);
-            }
-        }
-    }else if(iscntrl(input)){
-        printf("%d\r\n", input);
-        switch(input){
-            case 13:
-                // ENTER key pressed
-                printf("Message: %s\n", message_to_send);
-                free(message_to_send);
-                message_to_send = NULL;
-                length_message = -1;
+    if(escape_sequence[0] == 0){
+        on_command_mode = true;
+        printf("Switched to command move\n");
+    }else if(escape_sequence[0] == '['){
+        switch(escape_sequence[1]){
+            case 'A':
+                // up arrow
+                printf("up arrow\r\n");
                 break;
+            case 'C':
+                // right arrow
+                printf("Right arrow\r\n");
+                break;
+            case 'B':
+                // down arrow
+                printf("down arrow\r\n");
+                break;
+            case 'D':
+                // left arrow
+                printf("left arrow\r\n");
+                break;
+            default:
+                printf("27 - %d - %d\r\n", escape_sequence[0], escape_sequence[1]);
         }
-    }else{
-        // ASCII [32, 126]
-        if(on_command_mode){
-            // User pressed ESCAPE, so the next keystroke (which is the value of "input") determines what to do
-            switch(input){
-                case 'i':
-                    // Continue writing the message
-                    break;
-            }
-        }else{
-            // Allocate heap for string user is typing
-            if(length_message == -1) {
-                printf("Init\n");
-                message_to_send = calloc(MAX_LENGTH_MESSAGE, sizeof(char));
-                length_message = 0;
-            }
+    }
+}
 
-            if(length_message != MAX_LENGTH_MESSAGE - 1){
-                // Minus 1 since MAX_LENGTH_MESSAGE includes end of string character
-                message_to_send[length_message++] = input;
-                printf("message temp: %s\n", message_to_send);
-            }
+void handle_input(char input){
+    switch(input){
+        case 0:
+            // No input given
+            break;
+        case 13: {
+            // ENTER Key pressed
+            printf("Message: %s\n", message_to_send);
+            free(message_to_send);
+            message_to_send = NULL;
+            length_message = -1;
+            break;
         }
-        printf("%d ('%c')\r\n", input, input);
+        case 27:
+            // ESCAPE key: escape key pressed or escape sequence (arrow keys)
+            handle_escape();
+            break;
+        default:
+            // ASCII codes [32, 126]: All printable ASCII characters
+            if(on_command_mode){
+                // User pressed ESCAPE, so the next keystroke (which is the value of "input") determines what to do
+                switch(input){
+                    case 'i':
+                        // Continue writing the message
+                        break;
+                }
+            }else{
+                // Allocate heap for string user is typing
+                if(length_message == -1) {
+                    printf("Init\n");
+                    message_to_send = calloc(MAX_LENGTH_MESSAGE, sizeof(char));
+                    length_message = 0;
+                }
+
+                if(length_message != MAX_LENGTH_MESSAGE - 1){
+                    // Minus 1 since MAX_LENGTH_MESSAGE includes end of string character
+                    message_to_send[length_message++] = input;
+                    printf("message temp: %s\n", message_to_send);
+                }
+            }
+            printf("%d ('%c')\r\n", input, input);
     }
 }
